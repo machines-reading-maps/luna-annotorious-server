8ee9ce817d80a67a5d8229b7241f9c64e0ad9219
Rainer Simon <aboutgeo@gmail.com>
2022-08-17 15:22:29 +0200

    Cleanup

--- api/test/API.test.js
+++ api/test/API.test.js
@@ -4,13 +4,35 @@ import API from '../src/API.js';
 
 import MockDB from './MockDB.js';
 
-test('requests the "/" route', async t => {
+test('requests the "/annotation/search" route', async t => {
   const server = API(MockDB);
 
   const response = await server.inject({
     method: 'GET',
-    url: '/'
+    url: '/annotation/search?source=http://example.com/images/001'
   });
 
-  t.equal(response.statusCode, 404, 'returns a status code of 200');
+  t.equal(response.statusCode, 200, 'returns a status code of 200');
+});
+
+test('requests the "/annotation" POST method', async t => {
+  const server = API(MockDB);
+
+  const response = await server.inject({
+    method: 'POST',
+    url: '/annotation'
+  });
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200');
+});
+
+test('requests the "/annotation" DELETE method', async t => {
+  const server = API(MockDB);
+  
+  const response = await server.inject({
+    method: 'DELETE',
+    url: '/annotation/ceabd881-06cb-4d6e-a5c6-3490a7931bc7'
+  });
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200');
 });
