1803500a5be905d80f2a8f219cfe52764f6d7564
Rainer Simon <aboutgeo@gmail.com>
2022-08-19 09:24:25 +0200

    Proof of concept: JWT/Cookie-based auth

--- api/src/API.js
+++ api/src/API.js
@@ -1,16 +1,48 @@
 import Fastify from 'fastify';
+import JWT from '@fastify/jwt';
+import Cookie from '@fastify/cookie';
 
 const API = (db, opts = {}) => {
   const server = Fastify(opts);
 
-  server.get('/annotation/search', request =>
-    db.findBySource(request.query.source));
+  server.register(Cookie);
 
-  server.post('/annotation', request =>
-    db.upsertAnnotation(request.body).then(() => ({ result: 'success' })));
+  server.register(JWT, {
+    secret: 'supersecret',
+    cookie: {
+      cookieName: 'token',
+      signed: false
+    }
+  });
+  
+  server.addHook('onRequest', async (request, reply) => {
+    try {
+      // Adds user info to request object if user is logged in
+      await request.jwtVerify();
+    } catch (err) {
+      // Do nothing (anonymous user)
+    }
+  });
 
-  server.delete('/annotation/:annotationId', request =>
-    db.deleteById(request.params.annotationId).then(() => ({ result: 'success' })));
+  /** Just gets a dummy token for testing **/
+  server.get('/login', (req, res) => {
+    const token = server.jwt.sign({ id: 'https://rainersimon.io/users/rainer', fullname: 'Rainer Simon' });
+
+    res.setCookie('token', token, {
+      // TODO needs extra params for security!
+    }).code(200).send({ token });
+  });
+
+  server.get('/annotation/search', req =>
+    db.findBySource(req.query.source));
+
+  server.post('/annotation', req =>
+    db.upsertAnnotation(req.body).then(() => ({ result: 'success' })));
+
+  server.delete('/annotation/:annotationId', req =>
+    db.deleteById(req.params.annotationId).then(() => ({ result: 'success' })));
+
+  server.get('/', req => ({ ...req.user }));
 
   return server;
 }
